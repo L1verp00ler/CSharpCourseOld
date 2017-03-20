61650a8c79f9051fd96aae4a06b7735a2d6fd4d7
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-03-21 00:53:57 +0300

    - 1-ая часть домашнего задания (вывод наименьшего числа из 3-ех) - заготовка под вторую часть дз

--- lesson1_ConsoleApp1/Program.cs
+++ lesson1_ConsoleApp1/Program.cs
@@ -10,6 +10,60 @@ namespace lesson1_ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Введите число №" + (i+1));
+                numbers[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            /*
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            int min = Min(numbers);
+            Console.WriteLine("Наименьшее число: " + min);
+            Console.WriteLine("Последовательности Фиббоначчи:");
+
+            //int number1 = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Число равно {0}", number1);
+            Console.ReadLine();
         }
+
+        static int Min(int[] numbersArr)
+        {
+            int min;
+            if (numbersArr[0] < numbersArr[1] && numbersArr[0] < numbersArr[2])
+            {
+                min = numbersArr[0];
+            } else if (numbersArr[1] < numbersArr[2])
+            {
+                min = numbersArr[1];
+            } else
+            {
+                min = numbersArr[2];
+            }
+            return min;
+        }
+
+        static int Fibb(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            } else
+            {
+                return (Fibb(n - 1) + Fibb(n - 2));
+            }
+        }
+
     }
 }
