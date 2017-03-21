9f80306240aa7d716b6766703d1b72d2154bfc60
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-03-21 13:52:38 +0300

    Мусорка

--- lesson1_ConsoleApp1/Program.cs
+++ lesson1_ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace lesson1_ConsoleApp1
         static void Main(string[] args)
         {
             int[] numbers = new int[3];
+            //int[] numbers = new int[] { 3, 10, 1 };
 
             for (int i = 0; i < 3; i++)
             {
@@ -29,9 +30,62 @@ namespace lesson1_ConsoleApp1
             Console.WriteLine("Наименьшее число: " + min);
             Console.WriteLine("Последовательности Фиббоначчи:");
 
+            // 0 1 1 2 3 5 8 13 - для 14
+            // 0 1 1 - для 2
+            // 0 1 1 2 3 - для 4
+
+            // если тек элем-т меньше входного числа, то мы добавляем его к строке "0 1 "
+
             //int number1 = Int32.Parse(Console.ReadLine());
             //Console.WriteLine("Число равно {0}", number1);
-            Console.ReadLine();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                FibbSequence(numbers[i]);
+                Console.WriteLine();
+            }
+
+            /*
+            string fibbStr = "01";
+            
+            int i = 2;
+            int tekElem;
+            
+            while (Fibb(i) < numbers[3])
+            {
+                fibbStr+=Fibb(i);
+                Console.WriteLine(fibbStr);
+                i++;
+            }
+            */
+
+            /*
+            /// ПОСЛЕДНЕЕ РАБОТАЮЩЕЕ РЕШЕНИЕ!!!
+            int i = 0;
+            int tekElem;
+            
+            while ((tekElem = Fibb(i)) < numbers[2])
+            {
+                Console.Write(tekElem); // Console.Write(Fibb(i));
+                i++;
+            }
+            */
+
+            //Console.WriteLine(fibbStr);
+
+            /*
+            for (int i=2; i < numbers[3]; i++)
+            {
+                int tekElem = Fibb(i);
+                
+                while (tekElem < numbers[3])
+                {
+                    fibbstr+=tekElem;
+                }
+            }
+            */
+
+            //Console.ReadLine();
         }
 
         static int Min(int[] numbersArr)
@@ -48,6 +102,43 @@ namespace lesson1_ConsoleApp1
                 min = numbersArr[2];
             }
             return min;
+        }
+
+        static void FibbSequence(int n)
+        {
+
+            int i = 0;
+            int tekElem;
+
+            while ((tekElem = Fibb(i)) < n)
+            {
+                Console.Write(tekElem); // Console.Write(Fibb(i));
+                i++;
+            }
+
+            /*
+            int i = 0;
+            while (Fibb(i) < 5)
+            {
+                return "a";
+            }
+            
+            return "b";
+            
+            /*
+            string result = "01";
+            
+            if (n == 1)
+            {
+                return "0";
+            } else {
+                for (int i = 2; i < n; i++)
+                {
+                    result
+                }
+            }
+            */
+
         }
 
         static int Fibb(int n)
