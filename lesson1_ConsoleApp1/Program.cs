bb6905675165db2f26443d870a41df0e3280edfe
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-03-21 15:27:48 +0300

    Сохранил мусор на память)

--- lesson1_ConsoleApp1/Program.cs
+++ lesson1_ConsoleApp1/Program.cs
@@ -93,5 +93,118 @@ namespace lesson1_ConsoleApp1
                 return (Fibb(n - 1) + Fibb(n - 2));
             }
         }
+
+        // Вывод последовательности Фиббоначчи, в которой последний элемент меньше заданного числа
+        /*
+        static void FibbSequence(int n)
+        {
+
+            int i = 0;
+            int tekElem;
+
+            while ((tekElem = Fibb(i)) < n) // while (Fibb(i) < n)
+            {
+                Console.Write(tekElem); // Console.Write(Fibb(i));
+                i++;
+            }
+
+            /*
+            int i = 0;
+            while (Fibb(i) < 5)
+            {
+                return "a";
+            }
+            
+            return "b";
+            
+            /*
+            string result = "01";
+            
+            if (n == 1)
+            {
+                return "0";
+            } else {
+                for (int i = 2; i < n; i++)
+                {
+                    result
+                }
+            }
+            //
+
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            // Получаем от пользователя 3 числа (сюда еще добавить проверку на ввод "левых" значений!!!)
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Введите число №" + (i + 1));
+                numbers[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            // Определяем наименьшее из 3-ёх введенных чисел и выводим его в консоль
+            int min = Min(numbers);
+            Console.WriteLine("Наименьшее число: " + min);
+
+            // Выводим последовательности Фиббоначчи, последний элемент каждой из которых меньше соответствующего из 3-ёх чисел (3 последовательности)
+            // 0 1 1 2 3 5 8 13 - для 14
+            // 0 1 1 - для 2
+            // 0 1 1 2 3 - для 4
+            Console.WriteLine("Последовательности Фиббоначчи:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                FibbSequence(numbers[i]);
+                Console.WriteLine();
+            }
+
+
+            // если тек элем-т меньше входного числа, то мы добавляем его к строке "0 1 "
+
+            /*
+            string fibbStr = "01";
+            
+            int i = 2;
+            int tekElem;
+            
+            while (Fibb(i) < numbers[3])
+            {
+                fibbStr+=Fibb(i);
+                Console.WriteLine(fibbStr);
+                i++;
+            }
+            //
+
+            /*
+            /// ПОСЛЕДНЕЕ РАБОТАЮЩЕЕ РЕШЕНИЕ!!!
+            int i = 0;
+            int tekElem;
+            
+            while ((tekElem = Fibb(i)) < numbers[2])
+            {
+                Console.Write(tekElem); // Console.Write(Fibb(i));
+                i++;
+            }
+            //
+
+            //Console.WriteLine(fibbStr);
+
+            /*
+            for (int i=2; i < numbers[3]; i++)
+            {
+                int tekElem = Fibb(i);
+                
+                while (tekElem < numbers[3])
+                {
+                    fibbstr+=tekElem;
+                }
+            }
+            //
+
+            Console.ReadLine();
+        }
+        */
     }
 }
